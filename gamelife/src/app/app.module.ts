5f5de92e0f0b2a01b35d56a4fbed9782565b6303
HippolyteMith <hippolyte.mithouard@gmail.com>
2022-11-21 15:58:43 +0100

    (feat) Component Inscription V 0.0.1 + dependances dans app.module

--- gamelife/src/app/app.module.ts
+++ gamelife/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { MatFormFieldModule} from '@angular/material/form-field'
+import { MatRadioModule} from '@angular/material/radio'
+import { MatInputModule} from '@angular/material/input'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
