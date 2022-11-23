bc3f15ce8324f61151c1f104ee3866df80c8ed3e
Jeyatheesan JEYASEGAR <jeyatheesan.jeyasegar@social.aston-ecole.com>
2022-11-23 10:04:29 +0100

    (feat) Ajout de produit et recherche dans service , Produits details et recherche-produit sont en cours de finition

--- gamelife/src/app/app.module.ts
+++ gamelife/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field'
 import { MatRadioModule} from '@angular/material/radio'
 import { MatInputModule} from '@angular/material/input'
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {HttpClientModule} from "@angular/common/http";
@@ -13,6 +12,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { RechercherProduitComponent } from './produit/rechercher-produit/rechercher-produit.component';
 import { MatCardModule } from "@angular/material/card";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -31,7 +31,8 @@ import { MatCardModule } from "@angular/material/card";
     MatFormFieldModule,
     MatRadioModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
