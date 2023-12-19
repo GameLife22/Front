771dd7da432d5d6dfb1fcad1efe3fb52085b31cb
Theesan <jjeyasegar@sqli.com>
2023-12-19 12:15:56 +0100

    Barre de recherche ameliorer

--- gamelife/src/app/produit/rechercher-produit/rechercher-produit.component.ts
+++ gamelife/src/app/produit/rechercher-produit/rechercher-produit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ProduitModel } from 'src/app/model/produit.model';
 import { ProduitService } from 'src/app/services/produit/produit.service';
 
@@ -15,49 +15,48 @@ export class RechercherProduitComponent implements OnInit {
   games: ProduitModel[] | any;
   searchFormGroup!: FormGroup;
 
+  constructor(private router: Router, private productService: ProduitService, private fb: FormBuilder) { }
 
-  constructor(private router : Router, private productService : ProduitService, private fb : FormBuilder) { }
-
-  /**
-   * Cette méthode permet au chargement du composant de récupérer
-   * les entrées effectuée sur la barre de recherche et
-   * d'appeler la méthode pour gérer la recheche de produit
-   * @author: Fabien
-   */
   ngOnInit(): void {
     this.searchFormGroup = this.fb.group({
-      keyword : this.fb.control('')
+      keyword: this.fb.control('')
     });
-  }
 
-  /**
-   * Cette méthode permet de gérer la recherche de produit,
-   * elle récupére le(s) jeu(x) vidéo(s) selon le terme recherché
-   * @author: Fabien
-   */
-
-  handleSearchProducts() {
-    let keyword = this.searchFormGroup.value.keyword;
-    this.productService.getProductsByName(keyword).subscribe({
-      next: (data) =>  {
-        this.games = data
-      },
-      error: (message) => {
-
+    this.searchFormGroup.get('keyword')?.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(keyword => {
+      if (keyword.trim() !== '') {
+        this.handleSearchProducts();
+      } else {
+        this.games = null;
       }
     });
   }
 
-  /**
-   * Cette méthode permet de rediriger vers une fiche produit
-   * @param product: un jeu vidéo
-   * @author: Fabien
-   */
-  goToProductSheet(product : ProduitModel) {
-    const link = ['/produit', product.id]
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() =>
-    this.router.navigate(link)
-    )
+  handleSearchProducts() {
+    let keyword = this.searchFormGroup.value.keyword;
+    this.productService.getProductsByName(keyword).subscribe({
+      next: (data) => {
+        this.games = data;
+      },
+      error: (message) => {
+        // Gérer les erreurs
+      }
+    });
   }
 
+  goToProductSheet(product: ProduitModel) {
+    const link = ['/produit', product.id];
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+      this.router.navigate(link)
+    );
+  }
+
+  onInputChange() {
+    // Réinitialiser les jeux lorsque le champ est vidé
+    if (this.searchFormGroup.value.keyword.trim() === '') {
+      this.games = null;
+    }
+  }
 }
