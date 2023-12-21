fefba6bf94a5ff6528ff75bdd28a3fb6793881b4
fabien bidault <fabienbidault@outlook.fr>
2023-12-21 15:10:21 +0100

    Delete material angular

--- gamelife/src/app/app.module.ts
+++ gamelife/src/app/app.module.ts
@@ -5,15 +5,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
 import { ProduitDetailComponent } from './produit/produit-detail/produit-detail.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { RechercherProduitComponent } from './produit/rechercher-produit/rechercher-produit.component';
-import { MatCardModule } from "@angular/material/card";
 import { AppRoutingModule } from './app-routing.module';
 import { TokenInterceptorProvider} from "./helpers/token.interceptor";
-import {MatSelectModule} from "@angular/material/select";
 import { FicheProduitComponent } from './produit/fiche-produit/fiche-produit.component';
 import { FooterComponent } from './partage/footer/footer.component';
 import { HeaderComponent } from './partage/header/header.component';
@@ -21,9 +17,6 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { InternalServerComponent } from './error-pages/internal-server/internal-server.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatInputModule} from "@angular/material/input";
 import {GestionCompteComponent} from "./gestion-compte/gestion-compte.component";
 import { PaimentComponent } from './paiment/paiment.component';
 import {NgxPayPalModule} from "ngx-paypal";
@@ -62,23 +55,15 @@ import { PanierComponent } from './panier/panier.component';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatRadioModule,
-    MatInputModule,
-    MatCardModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSelectModule,
     FormsModule,
-    MatSelectModule,
     NgxPayPalModule,
     AngularSvgIconModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
-    
+
   ],
   providers: [TokenInterceptorProvider],
   bootstrap: [AppComponent]
-}) 
+})
 export class AppModule { }
