b4f1e499526440b899f44af323b5c722eea86188
fabien bidault <fabienbidault@outlook.fr>
2024-05-25 11:41:14 +0200

    Montée de version de angular de 17 à 18

--- gamelife/src/app/services/admin/admin-login/admin-login.service.ts
+++ gamelife/src/app/services/admin/admin-login/admin-login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
 import {Observable} from "rxjs";
 
